6e61b37aa67ed9c27f276f22cff92d60d5ad1883
Oindil Golder <91556756+OINDIL@users.noreply.github.com>
2025-01-06 19:57:36 +0530

    building

--- src/app/(main)/agency/page.tsx
+++ src/app/(main)/agency/page.tsx
@@ -50,7 +50,7 @@ const page = async ({
   return (
     <div className="flex justify-center items-center mt-4">
       <div className="max-w-[850px] border-[1px] p-4 rounded-xl">
-        <h1 className="text-4xl">Create an Agency</h1>
+        <h1 className="text-4xl mb-3">Create an Agency</h1>
         <AgencyDetails
           data={{ companyEmail: authUser?.emailAddresses[0].emailAddress }}
         />
--- src/components/forms/AgencyDetails.tsx
+++ src/components/forms/AgencyDetails.tsx
@@ -11,12 +11,22 @@ import {
   CardHeader,
   CardTitle,
 } from "../ui/card";
-import { Form, FormControl, FormField, FormItem, FormLabel } from "../ui/form";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
 import { useForm } from "react-hook-form";
 import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import FileUpload from "../global/file-upload";
 import { Input } from "../ui/input";
+import { Switch } from "../ui/switch";
+import { NumberInput } from "@tremor/react";
 
 type Props = {
   data?: Partial<Agency>;
@@ -29,7 +39,7 @@ const FormSchema = z.object({
   companyEmail: z.string().min(1),
   companyPhone: z.string().min(1),
   whiteLabel: z.boolean(),
-  adress: z.string().min(1),
+  address: z.string().min(1),
   city: z.string().min(1),
   state: z.string().min(1),
   country: z.string().min(1),
@@ -53,7 +63,7 @@ const AgencyDetails = ({ data }: Props) => {
       companyEmail: data?.companyEmail,
       companyPhone: data?.companyPhone,
       whiteLabel: data?.whiteLabel || false,
-      adress: data?.address,
+      address: data?.address,
       city: data?.city,
       state: data?.state,
       country: data?.country,
@@ -104,7 +114,165 @@ const AgencyDetails = ({ data }: Props) => {
                       </FormControl>
                     </FormItem>
                   )}
-                ></FormField>
+                />
+                <div className="flex md:flex-row gap-4">
+                  <FormField
+                    disabled={isLoading}
+                    control={form.control}
+                    name="name"
+                    render={({ field }) => (
+                      <FormItem className="flex-1">
+                        <FormLabel>Agency Name</FormLabel>
+                        <FormControl>
+                          <Input {...field} placeholder="Enter Agency Name" />
+                        </FormControl>
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="companyEmail"
+                    render={({ field }) => (
+                      <FormItem className="flex-1">
+                        <FormLabel>Agency Email</FormLabel>
+                        <FormControl>
+                          <Input readOnly placeholder="Email" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </div>
+
+                <div className="flex md:flex-row gap-4">
+                  <FormField
+                    disabled={isLoading}
+                    control={form.control}
+                    name="companyPhone"
+                    render={({ field }) => (
+                      <FormItem className="flex-1">
+                        <FormLabel>Agency Phone Number</FormLabel>
+                        <FormControl>
+                          <Input placeholder="Phone" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </div>
+
+                <FormField
+                  disabled={isLoading}
+                  control={form.control}
+                  name="whiteLabel"
+                  render={({ field }) => {
+                    return (
+                      <FormItem className="flex flex-row items-center justify-between rounded-lg border gap-4 p-4">
+                        <div>
+                          <FormLabel>Whitelabel Agency</FormLabel>
+                          <FormDescription>
+                            Turning on whilelabel mode will show your agency
+                            logo to all sub accounts by default. You can
+                            overwrite this functionality through sub account
+                            settings.
+                          </FormDescription>
+                        </div>
+
+                        <FormControl>
+                          <Switch
+                            checked={field.value}
+                            onCheckedChange={field.onChange}
+                          />
+                        </FormControl>
+                      </FormItem>
+                    );
+                  }}
+                />
+                <FormField
+                  disabled={isLoading}
+                  control={form.control}
+                  name="address"
+                  render={({ field }) => (
+                    <FormItem className="flex-1">
+                      <FormLabel>Address</FormLabel>
+                      <FormControl>
+                        <Input placeholder="123 st..." {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <div className="flex md:flex-row gap-4">
+                  <FormField
+                    disabled={isLoading}
+                    control={form.control}
+                    name="city"
+                    render={({ field }) => (
+                      <FormItem className="flex-1">
+                        <FormLabel>City</FormLabel>
+                        <FormControl>
+                          <Input placeholder="City" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    disabled={isLoading}
+                    control={form.control}
+                    name="state"
+                    render={({ field }) => (
+                      <FormItem className="flex-1">
+                        <FormLabel>State</FormLabel>
+                        <FormControl>
+                          <Input placeholder="State" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    disabled={isLoading}
+                    control={form.control}
+                    name="zipCode"
+                    render={({ field }) => (
+                      <FormItem className="flex-1">
+                        <FormLabel>Zipcpde</FormLabel>
+                        <FormControl>
+                          <Input placeholder="Zipcode" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </div>
+                <FormField
+                  disabled={isLoading}
+                  control={form.control}
+                  name="country"
+                  render={({ field }) => (
+                    <FormItem className="flex-1">
+                      <FormLabel>Country</FormLabel>
+                      <FormControl>
+                        <Input placeholder="Country" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <div className="flex flex-col gap-2">
+                  <FormLabel>Create a goal</FormLabel>
+                  <FormDescription>
+                    ✨ Create a goal for your agency. As your business grows
+                    your goals grow too so dont forget to set the bar higher!
+                  </FormDescription>
+                  <NumberInput
+                    defaultValue={data?.goal}
+                    onChange={async () => {
+                      //!WIP: later
+                    }}
+                  ></NumberInput>
+                </div>
               </form>
             </Form>
           </CardContent>
